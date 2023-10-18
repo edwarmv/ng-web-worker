922f9243567170eed46e21945d1b0fef4403d205
Edwar Martinez Vale <edwar.martinez@bupunku.com>
2023-10-18 13:17:40 -0400

    feat: use the angular cli to generate a web worker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { TestWorkerService } from './services/test-worker.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ng-web-worker';
+
+  constructor(private _testWorkerService: TestWorkerService) {}
 }
